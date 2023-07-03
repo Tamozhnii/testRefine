d578f4b0079702b645aa1d3607f7140dcdcd4348
Petr <46220413+Tamozhnii@users.noreply.github.com>
2023-07-03 17:12:10 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import {
+    notificationProvider,
+    WelcomePage,
+    ErrorComponent,
+    RefineThemes,
+} from "@refinedev/antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+
+import { ConfigProvider } from "antd";
+import "@refinedev/antd/dist/reset.css";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(
+                        "https://api.fake-rest.refine.dev",
+                    )}
+                    notificationProvider={notificationProvider}
+                    options={{
+                        syncWithLocation: true,
+                        warnWhenUnsavedChanges: true,
+                    }}
+                >
+                    <Routes>
+                        <Route index element={<WelcomePage />} />
+
+                        <Route path="*" element={<ErrorComponent />} />
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                    <DocumentTitleHandler />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+}
+
+export default App;
